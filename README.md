5f815783da4f645c4710c817062fd4dca65625e7
E. H. Rast <erich@snafu.de>
2022-06-07 14:57:22 +0100

    Changed API and use normal pointer receivers. Added example.

--- README.md
+++ README.md
@@ -5,3 +5,32 @@ a Go container combining a map with a reverse map
 [![Go Report Card](https://goreportcard.com/badge/github.com/rasteric/doublemap)](https://goreportcard.com/report/github.com/rasteric/doublemap)
 
 This package provides a map with a reverse map mechanism, i.e., it allows you to get the value by a given key and a key from a given value.
+
+## Example usage
+
+```
+package main
+
+import (
+  "fmt"
+
+  "github.com/rasteric/doublemap"
+)
+
+func main() {
+	m := doublemap.New[string, int]()
+    m.Set("first", 1)
+    m.Set("second", 2)
+    m.Set("third", 3)
+    v, _ := m.Get("first")
+    fmt.Println(v)
+    k, _ := m.ByValue(3)
+    fmt.Println(k)
+}
+```
+
+See the reference for more information.
+
+## License
+
+This package is provided under the permissive MIT License, please see the accompanying LICENSE agreement for more information.
--- map.go
+++ map.go
@@ -1,46 +1,25 @@
 // Package doublemap provides a generic Map[K comparable, V comparable] with operations for getting and setting
 // values by key, and the corresponding reverse map operation of getting and setting keys by values. The Map is
 // not thread-safe.
-//
-// Example usage:
-//     import "github.com/rasteric/doublemap"
-//
-//     func main() {
-//       m := doublemap.Map[string]int
-//       m.Set("hello", 1)
-//       m.Set("world", 2)
-//       m.SetByValue(3, "third")
-//       m.Walk(func(key string, value int) bool {
-//         fmt.Sprintf("%v, %v\n", key, value)
-//         return true // will stop if return false
-//       })
-//     }
 package doublemap
 
 // A Map stores keys and values in a way that makes reverse mapping from values to keys efficient at the
-// cost of additional memory and storage complexity. A Map does not have to be initialized, you can use it
-// right out of the box. However, Map objects are not thread-safe.
+// cost of additional memory and storage complexity. You should only use this map if your values are unique
+// - otherwise the value-related lookup functions make no sense and Remove might have unexpected results!
 type Map[K comparable,V comparable] struct {
 	kv map[any]any
 	vk map[any]any
 }
 
-func (m Map[K, V]) maybeInit() bool {
-	if m.kv != nil {
-		return false
-	}
-  m.kv = make(map[any]any)
-	m.vk = make(map[any]any)
-	return true
+// New creates a new double map.
+func New[K, V comparable]() *Map[K, V] {
+	return &Map[K, V]{ kv: make(map[any]any), vk: make(map[any]any) }
 }
 
 // Get returns the value for the given key and true, the null value of the value type and false if no value
 // was stored for this key.
-func (m Map[K, V]) Get(key K) (V, bool) {
+func (m *Map[K, V]) Get(key K) (V, bool) {
   var result V
-	if m.maybeInit() {
-		return result, false
-	}
 	var ok bool
 	var x any
 	x, ok = m.kv[key]
@@ -51,15 +30,14 @@ func (m Map[K, V]) Get(key K) (V, bool) {
 }
 
 // Set sets a value for the given key. 
-func (m Map[K, V]) Set(key K, value V) {
-	m.maybeInit()
+func (m *Map[K, V]) Set(key K, value V) {
 	m.kv[key] = value
 	m.vk[value] = key
 }
 
 // Remove removes the key and value mapping based on the given key. True is returned if the mapping was removed,
 // false is returned when there was no mapping for the key in the first place.
-func (m Map[K, V]) Remove(key K) bool {
+func (m *Map[K, V]) Remove(key K) bool {
 	value, ok := m.Get(key)
 	if ok {
 		delete(m.kv, key)
@@ -71,11 +49,8 @@ func (m Map[K, V]) Remove(key K) bool {
 
 // ByValue returns the key for a given value and true, the key type's null value and false if no key was
 // stored for this value.
-func (m Map[K, V]) ByValue(value V) (K, bool) {
+func (m *Map[K, V]) ByValue(value V) (K, bool) {
 	var result K
-	if m.maybeInit() {
-		return result, false
-	}
 	var ok bool
 	var x any
 	x, ok = m.vk[value]
@@ -99,22 +74,18 @@ func (m Map[K, V]) RemoveByValue(value V) bool {
 
 // Copy creates a copy of the key-value mapping. This operation is fairly slow but faster than using Get and Set
 // manually. The copy is not deep, i.e., any key and values are just copied using ordinary assignment.
-func (m Map[K, V]) Copy() Map[K, V] {
+func (m *Map[K, V]) Copy() *Map[K, V] {
 	m2 := Map[K, V]{}
-	m2.maybeInit()
-	if m.maybeInit() {
-    return m2
-	}
 	for k, v := range m.kv {
 		m2.kv[k] = v
 		m2.vk[v] = k 
 	}
-	return m2
+	return &m2
 }
 
 // Walk traverses key-value pairs in the map and provides them to the given function in unspecified order
 // until the function returns false. 
-func (m Map[K, V]) Walk(fn func (key K, value V) bool) {
+func (m *Map[K, V]) Walk(fn func (key K, value V) bool) {
 	for k, v := range m.kv {
 		if !fn(k.(K), v.(V)) {
 			break
